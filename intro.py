1b8c2db1e8b34bb5db2abda9989fafd2a812b703
Noha Shehab <nohashehab.iti@gmail.com>
2025-11-11 11:04:06 +0200

    intro to python

---
+++ intro.py
@@ -0,0 +1,76 @@
+"This is a doc string "
+
+# this a comment
+name = 'Ahmed'  # loosely
+print(type(name))
+Name = "Ali"
+# writing python script must start from the beginning of the line
+
+res  =10
+# python is loosely dynamically typed lang. --> interpreter detect datatype in the runtime.
+name = 100
+print(type(name))
+
+# indentation
+if name=='ahmed':
+    print("Hi")
+else:
+    print("bye")
+
+
+if name:
+    print("hello")
+
+print("hello"); print("test")
+
+###############
+
+bio= ("My name is Noha"
+      "I lives in Mansoura")
+
+print(bio)
+
+######
+
+bio2= ("My name is Noha\n"
+      "I lives in Mansoura")
+
+print(bio2)
+####
+bio3= '''My name is Noha
+I works at ITI
+I lives in Mansoura'''
+print(bio3)
+
+
+"""you can use string without ref as a comment.
+any content ###  
+
+"""
+""" type conversion"""
+age = 25 # int
+print(type(age))
+
+# convert age to be string
+age = str(age)  # type casting
+print(type(age))
+
+char ="e"
+import sys
+print(sys.getsizeof(char))
+
+
+mystr="1243"
+print(type(mystr))
+mystr = int(mystr)
+
+##########
+str2 = "5iti"
+print(type(str2))
+# str2 = int(str2)
+# print(str2)
+
+print('iti' and "ai")
+
+
+
