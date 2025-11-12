3c900322e269a01502f6c5cb3dc4809a06ad5f82
Noha Shehab <nohashehab.iti@gmail.com>
2025-11-12 14:21:21 +0200

    lists-sets-dicts-functions

--- playground.py
+++ playground.py
@@ -1,8 +1,19 @@
 
-name = "ahmed"
-print(type(name))
-print(isinstance(name, object))
+# name = "ahmed"
+# print(type(name))
+# print(isinstance(name, object))
+#
+# msg = "We love ITI 423325"
+# msg = msg.split()
+# print(msg)
+# ss = {"sfsfd", "Aff", "Fsdfsf", True}
+# print(ss)
+# print(id(ss))
+# ss.add("new")
+# print(id(ss))
+#
+# for item in ss:
+#     print(item)
 
-msg = "We love ITI 423325"
-msg = msg.split()
-print(msg)
+name= "noha"
+salary = 10000
